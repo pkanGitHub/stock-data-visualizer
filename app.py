b4910749dd0f2c6634c6fe6d9524fbc66562cf85
Dwalden2021 <92900193+Dwalden2021@users.noreply.github.com>
2022-07-13 21:38:06 -0500

    Split JSON Object Function

--- app.py
+++ app.py
@@ -14,18 +14,22 @@ def get_url(symbol, timeSeries, API_KEY):
     return f"https://www.alphavantage.co/query?function={timeSeries}&symbol={symbol}&apikey={API_KEY}"
 
 def get_data(url):
-    #res = requests.get(get_url(url))
     res = requests.get(url)
     data = res.json()
-    
-    #Dump into text file
-    with open('jSonDump.text', 'w') as f:
-        f.write(json.dumps(data))
 
-    #Dump into csv file
-    df = pd.DataFrame.from_dict(data, orient='index')
-    df.to_csv('csvTest.csv', encoding='utf-8', index=False)
-
+    #print(json.dumps(data, indent=2))
+    #print(data.keys())
+    for key in data.keys():
+        if key != "Meta Data":
+            print(key,'\n',"-----------\n")
+            for x in data[key]:
+                for z in data[key][x]:
+                    #print(x)
+                    #print(z)
+                    #print(data[key][x][z])
+                    print(x, ":  ", z, " = ",data[key][x][z], sep="")
+                    if(z == "5. volume"):
+                        print("-----------")
     return data
 
 def main():
@@ -44,7 +48,7 @@ def main():
 
     url = get_url(symbol, timeSeries, API_KEY)
     data = get_data(url)
-    print(data)
+    #print(data)
     #print(url)
 
 main()
